93c897f0f9e00e1bb78db799114c631fcb6f341d
Prakash180 <70065693+Prakash180@users.noreply.github.com>
2021-05-17 16:40:03 +0530

    Update urls.py

--- heart_disease_p/urls.py
+++ heart_disease_p/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
 from home import views
-
+from django.views.static import serve
 
 urlpatterns = [
     path('admin', admin.site.urls),
